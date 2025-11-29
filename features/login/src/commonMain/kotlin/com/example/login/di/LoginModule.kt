55db429c5ad5d7a818414ae57ed4ed00bd179144
Jesús Manuel García Blanco <jesus.garciab@sngular.com>
2025-11-29 17:24:10 +0100

    refactored LoginFlow with LoginScreen

--- features/login/src/commonMain/kotlin/com/example/login/di/LoginModule.kt
+++ features/login/src/commonMain/kotlin/com/example/login/di/LoginModule.kt
@@ -1,7 +1,7 @@
 package com.example.login.di
 
 import com.example.login.api.LoginRoute
-import com.example.login.presentation.LoginFlow
+import com.example.login.presentation.LoginScreen
 import com.example.navigation.Navigator
 import com.example.registerapi.RegisterRoute
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -13,7 +13,7 @@ import org.koin.dsl.navigation3.navigation
 val loginModule = module {
     navigation<LoginRoute> {
         val navigator: Navigator = get()
-        LoginFlow(
+        LoginScreen(
             onLoginSuccess = {navigator.login()},
             onRegisterClick = {navigator.navigateTo(RegisterRoute)},
         )
--- features/login/src/commonMain/kotlin/com/example/login/presentation/LoginScreen.kt
+++ features/login/src/commonMain/kotlin/com/example/login/presentation/LoginScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoginFlow(
+fun LoginScreen(
     onLoginSuccess: () -> Unit,
     onRegisterClick: () -> Unit,
 ) {
